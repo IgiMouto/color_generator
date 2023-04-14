0ece9f985a068203dbdc640cbcc3bf4284d6bfe4
Luigi Angelo Siguenza <igi.siguenza@gmail.com>
2023-04-14 14:39:44 +0800

    :tada: Upload Project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:color_generator/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HomePage());
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: public_member_api_docs
+
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color color = Colors.white;
+  int baseColor = 0xFFFFFF;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GestureDetector(
+          onTap: () {
+            setState(() {
+              color = Color((math.Random().nextDouble() * baseColor).toInt())
+                  .withOpacity(1.0);
+            });
+          },
+          child: ColoredBox(
+            color: color,
+            child: const Center(
+              child: Text(
+                "Hello there",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
